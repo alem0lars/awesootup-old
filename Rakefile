b7306f6b6bd7591acc007e893b9f12d818d7a9ac
Alessandro Molari <molari.alessandro@gmail.com>
2013-02-25 10:27:31 +0100

    index page in progress

--- Rakefile
+++ Rakefile
@@ -1,1 +1,17 @@
 require 'nanoc3/tasks'
+
+task :watch do
+  sh 'bundle exec nanoc watch'
+end
+
+task :compile do
+  sh 'bundle exec nanoc compile'
+end
+
+task :view do
+  sh 'bundle exec nanoc view'
+end
+
+task :irb do
+  sh 'bundle exec irb'
+end
--- content/assets/scripts/app/moving.js
+++ content/assets/scripts/app/moving.js
@@ -1,5 +1,20 @@
 define(['jquery'], function($) {
+
   $(".go-to-bottom").click(function() {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    $("html, body").animate({
+      scrollTop: $(document).height()
+    }, "slow");
   });
+
+  $(".scroll-to").click(function() {
+    var $this = $(this);
+    $('html, body').animate({
+      scrollTop: $("#" + $this.data("scrollTo")).offset().top
+    }, "slow");
+  });
+
+  $('.select-tab').click(function() {
+    $("#" + $(this).data("selectTab")).click();
+  });
+
 });
